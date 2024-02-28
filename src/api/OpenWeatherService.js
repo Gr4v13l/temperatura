d6b5fe1394349af5f61eaaa2a443a9516b314fd1
joão victor <Joaovictorrrm@outlook.com>
2024-02-28 19:49:30 -0300

    temperatura

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = '707ab5d58f0a727f6c797984145327e1r';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
--- src/utilities/ApiService.js
+++ src/utilities/ApiService.js
@@ -1,6 +1,6 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'YOUR API KEY';
+const WEATHER_API_KEY = '707ab5d58f0a727f6c797984145327e1r';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
